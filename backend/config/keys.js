71bdc0c7ff999f01aba7e7988f09e54094ccff8d
Yaqi Fang <cfang322@gmail.com>
2024-01-27 18:17:46 -0500

    Good: finish express server security

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
 // process.env is an object that stores all the environment variables as key-value pairs.
 module.exports = {
-    mongoURI: process.env.MONGO_URI
+    mongoURI: process.env.MONGO_URI,
+    isProduction: process.env.NODE_ENV === 'production'
 }
