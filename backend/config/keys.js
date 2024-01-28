a34e051f7f77f03d64d9b3ea57bd244f3ae796a5
Yaqi Fang <cfang322@gmail.com>
2024-01-27 19:34:05 -0500

    complete passport JWT setup

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,6 @@
 // process.env is an object that stores all the environment variables as key-value pairs.
 module.exports = {
+    secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
     isProduction: process.env.NODE_ENV === 'production'
 }
