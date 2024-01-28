674625973700cd7968977ee81610bb873678b204
Yaqi Fang <cfang322@gmail.com>
2024-01-27 19:09:40 -0500

    Good: finish testing functionality

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 require('./models/User');
+require('./config/passport');
+const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -18,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
 // Security Middleware
 if (!isProduction) {
