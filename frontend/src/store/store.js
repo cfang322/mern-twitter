89afecb23a49712b173b82926a6ffe1141dba98e
Yaqi Fang <cfang322@gmail.com>
2024-01-27 23:09:53 -0500

    Good: fronend setup continued; finish setup for redux cycle

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 const rootReducer = combineReducers({
   // Add reducers here
@@ -7,16 +7,16 @@ const rootReducer = combineReducers({
 
 let enhancer;
 if (import.meta.env.MODE === "production") {
-  enhancer = applyMiddleware(thunk);
-} else {
-  const logger = (await import("redux-logger")).default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+        enhancer = applyMiddleware(thunk);
+    } else {
+        const logger = (await import("redux-logger")).default;
+        const composeEnhancers =
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
 const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+    return createStore(rootReducer, preloadedState, enhancer);
 };
 
 export default configureStore;
